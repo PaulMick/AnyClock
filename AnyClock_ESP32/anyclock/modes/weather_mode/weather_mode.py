dc1eefc50520825d82ac52c387983e9900e32252
PaulMick <paulwmick@gmail.com>
2025-08-12 11:23:14 -0600

    moving stuff

--- AnyClock_ESP32/anyclock/modes/weather_mode/weather_mode.py
+++ AnyClock_ESP32/anyclock/modes/weather_mode/weather_mode.py
@@ -1,5 +1,8 @@
 import time
 
+# How often weather data is pulled from the API, in seconds
+WEATHER_UPDATE_PERIOD = 60
+
 class WeatherMode:
     def __init__(self, night: bool = False) -> None:
         self.night = night
@@ -8,6 +11,9 @@ class WeatherMode:
         self.tempc: int = 0
         self.weather_code: int = 0
         self.humidity: int = 0
+
+        self.year: int = 0
+        self.month: int = 0
         
     def update(self, pressed: tuple[int], held: tuple[int]) -> bool:
         pass
