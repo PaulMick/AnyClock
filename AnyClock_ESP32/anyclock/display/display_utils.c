c91fbca29570baba860349b59f44156d12d964d7
PaulMick <paulwmick@gmail.com>
2025-08-17 22:32:52 -0600

    added more drawing functions

--- AnyClock_ESP32/anyclock/display/display_utils.c
+++ AnyClock_ESP32/anyclock/display/display_utils.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #define DISPLAY_WIDTH 64
 #define DISPLAY_HEIGHT 32
@@ -13,25 +14,97 @@ const char *week_days_short[7] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN
 
 const uint32_t font[128];
 
-void draw_char(uint8_t *frame_buf[DISPLAY_HEIGHT][DISPLAY_WIDTH][3], uint32_t font_char, int width, int x, int y, uint8_t r, uint8_t g, uint8_t b) {
+enum direction {
+    DIRECTION_UP,
+    DIRECTION_DOWN,
+    DIRECTION_LEFT,
+    DIRECTION_RIGHT
+};
+
+// Safe function for drawing a single pixel, checks if pixel coordinates are on the display before writing
+void draw_pixel(uint8_t *frame_buf[DISPLAY_HEIGHT][DISPLAY_WIDTH][3], int x, int y, uint8_t r, uint8_t g, uint8_t b) {
+    if (x >= 0 && x < DISPLAY_WIDTH && y >= 0 && y < DISPLAY_HEIGHT) {
+        *frame_buf[y][x][0] = r;
+        *frame_buf[y][x][1] = g;
+        *frame_buf[y][x][2] = b;
+    }
+}
+
+// Draw a single ASCII character
+// x, y are the top-left corner
+int draw_char(uint8_t *frame_buf[DISPLAY_HEIGHT][DISPLAY_WIDTH][3], char c, int x, int y, uint8_t r, uint8_t g, uint8_t b) {
+    uint32_t font_char = font[c];
+    int width = font_char >> 25;
     for (int i = 0; i < width; i ++) {
         for (int j = 0; j < 5; j ++) {
-            if (font_char & 1 << j + 5 * i) {
-                *frame_buf[y + j][i + x][0] = r;
-                *frame_buf[y + j][i + x][1] = g;
-                *frame_buf[y + j][i + x][2] = b;
+            draw_pixel(frame_buf, x + i, y + j, r, g, b);
+        }
+    }
+    return width;
+}
+
+// Draw a string of ASCII characters
+// x, y are the top-let corner
+void draw_str(uint8_t *frame_buf[DISPLAY_HEIGHT][DISPLAY_WIDTH][3], char *str, int x, int y, int spacing, uint8_t r, uint8_t g, uint8_t b) {
+    for (int i = 0; i < strlen(str); i ++) {
+        int width = draw_char(frame_buf, str[i], x, y, r, g, b);
+        x += width + spacing;
+    }
+}
+
+// Draws a rectangle in the specified location
+// x, y are the top left corner
+// thickness is the thickness of the perimeter, -1 for completely filled in
+void draw_rect(uint8_t *frame_buf[DISPLAY_HEIGHT][DISPLAY_WIDTH][3], int x, int y, int width, int height, int thickness, uint8_t r, uint8_t g, uint8_t b) {
+    for (int i = 0; i < width; i ++) {
+        for (int j = 0; j < height; j ++) {
+            if (thickness == -1 || i < thickness || i > width - thickness || j < thickness || j > height - thickness) {
+                draw_pixel(frame_buf, x + i, y + j, r, g, b);
             }
         }
     }
 }
 
-void draw_str(uint8_t *frame_buf[DISPLAY_HEIGHT][DISPLAY_WIDTH][3], char *str, int x, int y, uint8_t r, uint8_t g, uint8_t b, int spacing) {
-    
+// Draws a straight line in one of the four cardinal directions
+void draw_line(uint8_t *frame_buf[DISPLAY_HEIGHT][DISPLAY_WIDTH][3], int x, int y, enum direction direction, int length, uint8_t r, uint8_t g, uint8_t b) {
+    switch (direction) {
+        case DIRECTION_UP:
+            for (int i = 0; i < length; i ++) {
+                draw_pixel(frame_buf, x, y - i, r, g, b);
+            }
+            break;
+        case DIRECTION_DOWN:
+            for (int i = 0; i < length; i ++) {
+                draw_pixel(frame_buf, x, y + i, r, g, b);
+            }
+            break;
+        case DIRECTION_LEFT:
+            for (int i = 0; i < length; i ++) {
+                draw_pixel(frame_buf, x - i, y, r, g, b);
+            }
+            break;
+        case DIRECTION_RIGHT:
+            for (int i = 0; i < length; i ++) {
+                draw_pixel(frame_buf, x + i, y, r, g, b);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+// Fill the entire display with one color
+void fill_dislpay(uint8_t *frame_buf[DISPLAY_HEIGHT][DISPLAY_WIDTH][3], uint8_t r, uint8_t g, uint8_t b) {
+    for (int i = 0; i < DISPLAY_WIDTH; i ++) {
+        for (int j = 0; j < DISPLAY_HEIGHT; j ++) {
+            *frame_buf[j][i][0] = r;
+            *frame_buf[j][i][1] = g;
+            *frame_buf[j][i][2] = b;
+        }
+    }
 }
 
 // TODO functions:
-// draw rect (x, y, width, height, color)
-// draw line (x1, y1, x2, y2, color)
 // draw bmp img (bmp, x, y)
 
 int load_font(char *fname) {
