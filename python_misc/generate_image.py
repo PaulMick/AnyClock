58121fd3798f5480c897c46b443c9816240f128b
PaulMick <paulwmick@gmail.com>
2025-08-22 22:57:16 -0400

    working on image display

--- python_misc/generate_image.py
+++ python_misc/generate_image.py
@@ -1,12 +1,19 @@
 import numpy as np
 import cv2
 
-animation = input("Animation? (y/n): ")
+# width and height of the image
+WIDTH = 7
+HEIGHT = 5
 
-if animation == "y":
-    animation_name = input("Animation folder name: ")
-    fps = float(input("Frames per second: "))
-else:
-    image_name = input("Image name (include extension): ")
-    cv_im = cv2.LoadImage(image_name)
-    np_im = np.asarray(cv_im)
+fname = input("Image name (exclude .png extension): ")
+cv_im = cv2.imread(f"python_misc/image_input/{fname}.png")
+np_im = np.asarray(cv_im)
+
+with open(f"python_misc/image_output/{fname}.bmpimg", "wb") as f:
+    f.write(WIDTH.to_bytes(1, "big"))
+    f.write(HEIGHT.to_bytes(1, "big"))
+    for i in range(WIDTH):
+        for j in range(HEIGHT):
+            f.write(int(np_im[j, i, 0]).to_bytes(1, "big"))
+            f.write(int(np_im[j, i, 1]).to_bytes(1, "big"))
+            f.write(int(np_im[j, i, 2]).to_bytes(1, "big"))
